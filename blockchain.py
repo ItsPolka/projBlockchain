ab8e061e9ddf8e60a0e690f65d2d2861a6164f5a
ItsPolka <fernando21.revilla@gmail.com>
2026-09-22 11:28:02 -0600

    design change

--- blockchain.py
+++ blockchain.py
@@ -9,20 +9,24 @@ https://medium.com/@katakakikita/build-your-own-blockchain-in-python-a-practical
 
 class Blockchain:
     def __init__(self):
-        self.transaction ={}
-        self.chain = []
-        self.nodes = set()
+        self.index=index
+        self.timestamp=timestamp
+        self.transaction=transaction
+        self.previous_hash = previousHash
+        self.nonce = 0
+        self.hash = self.calculateHash()
 
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self, proof, previous_hash=None, sender, recipient, amount):
+    def new_block(self, index,timestamp,transaction,previousHash,nonce):
+
 
         pass
 
 
     @staticmethod
-    def hash(block):
+    def calculateHash(block):
         """
         Creates a SHA-256 hash of a Block
 
