c371a7bb9afb453f65159b638c1389be43e6e95d
Salomón Cherem <salomoncheremc2@gmail.com>
2026-09-24 11:08:59 -0600

    blockchain y block medio sirven

--- block.py
+++ block.py
@@ -2,6 +2,7 @@ import hashlib
 import json
 from time import time
 
+MAX_NONCE = 2 ** 200
 
 class Block:
     def __init__(self, index, transaction, previous_hash):
@@ -23,15 +24,26 @@ class Block:
         block_string = json.dumps(block_content, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
-    def mine_block(self, difficulty):
+    def mine_block(self, difficulty: int, blockchain):
         target = "0" * difficulty
         while self.hash[:difficulty] != target:
+            if self.nonce > MAX_NONCE:
+                self.timestamp = time()
+                self.nonce = 0
+            
             self.nonce += 1
             self.hash = self.calculate_hash()
+
+            print("Nonce:", self.nonce, "Hash:", self.hash)
+
+            if len(blockchain.chain) > self.index:
+                return False
+        return True
 
     def __repr__(self):
         return (
             f"Block(index={self.index}, hash={self.hash[:10]}..., "
             f"previous_hash={self.previous_hash[:10] if self.previous_hash else 'None'}..., "
-            f"nonce={self.nonce})"
+            f"nonce={self.nonce} "
+            f"transaction={self.transaction})"
         )
--- blockchain.py
+++ blockchain.py
@@ -11,18 +11,32 @@ https://medium.com/@katakakikita/build-your-own-blockchain-in-python-a-practical
 MAX_NONCE = 2 ** 200
 
 class Blockchain:
-    def __init__(self):
+    def __init__(self, difficulty: int):
         self.current_transactions = []
         self.chain = []
         self.nodes = set()
+        self.difficulty = difficulty
 
     def new_block(self, transaction):
-        nblock = Block(len(self.chain), transaction, Block.calculate_hash(self.last_block))
-        self.chain.append(nblock)
+        previous_hash = self.last_block.hash if self.chain else "0"
+        nblock = Block(len(self.chain), transaction, previous_hash)
+        success = nblock.mine_block(self.difficulty, self)
+        if success:
+            self.chain.append(nblock)
+            return nblock
+        return None
 
     @property
-    def last_block(self):
+    def last_block(self) -> Block:
         """
         Returns the last Block in the chain
         """
         return self.chain[-1]
+
+def main():
+    blockchain = Blockchain(4)
+    blockchain.new_block("Alice le manda 1BTC a Bob")
+    print(blockchain.last_block)
+
+if __name__ == "__main__":
+    main()
